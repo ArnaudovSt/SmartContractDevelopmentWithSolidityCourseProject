20c62d4abe8e141bd95871a818248096fc532dd7
ArnaudovSt <telerikacademy2016@gmail.com>
2018-02-10 00:31:37 +0200

    added abstractions

---
+++ contracts/abstractions/IDDNSCore.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.18;
+
+interface IDDNSCore {
+    function registerDomain(bytes32 _domainName, bytes32 _ipAddress) public;
+
+    function editDomainIp(bytes32 _domainName, bytes32 _newIpAddress) public;
+
+    function transferOwnership(bytes32 _domainName, address _to) public;
+}
