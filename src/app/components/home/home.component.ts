d4e42c30f3b640aefccb27dfa1ec84a45f7c2289
ArnaudovSt <telerikacademy2016@gmail.com>
2018-03-10 05:48:15 +0200

    Added home component implementation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContractService } from '../../services/contract.service';
+import { Web3Service } from '../../services/web3.service';
 
 @Component({
 	selector: 'app-home',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	constructor() { }
+	contractInfo = {
+		wallet: '',
+		owner: '',
+		registrationCost: '',
+		expiryPeriod: ''
+	}
+
+	isContractDestroyed = false;
+
+	constructor(private contractService: ContractService, private web3Service: Web3Service) { }
 
 	ngOnInit() {
+		this._getContractInfo();
+	}
+
+	private async _getContractInfo() {
+		this.contractInfo.wallet = await this.contractService.getWallet();
+		this.contractInfo.owner = await this.contractService.getOwner();
+		this.contractInfo.registrationCost = await this.contractService.getRegistrationCost();
+		this.contractInfo.expiryPeriod = await this.contractService.getExpiryPeriodInDays();
+		if (Number(this.contractInfo.owner) === 0) {
+			this.isContractDestroyed = true;
+			return;
+		}
 	}
 
 }
